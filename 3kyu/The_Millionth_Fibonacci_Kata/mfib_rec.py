d986afad25c366040b0e5c9d021c068de32ff66d
Petar Jager <petar.jager@gmail.com>
2015-02-16 13:38:30 +0100

    3kyu mil fib work...

--- 3kyu/The_Millionth_Fibonacci_Kata/mfib_rec.py
+++ 3kyu/The_Millionth_Fibonacci_Kata/mfib_rec.py
@@ -32,27 +32,39 @@ def mfib_rec_bottom_up(n):
         mp=[mp[1],mp[2],mp[1]+mp[2]]
     return mp[-1]
 
+def mfib_rec_bottom_up_1(n):
+    mp=[0,1] 
+    if 0<=n<=1: return mp[n]
+    mp0=0
+    mp1=1
+    for i in xrange(n-2):
+        mp2=mp1+mp0
+        mp0=mp1
+        mp1=mp2
+    return mp2
+
 def mfib_rec(n):
     res=0
     start_t=time.time()
     #res=0-mfib_rec_top_down(abs(n)) if n<0 and n%2==0 else mfib_rec_top_down(abs(n))
     #res=0-mfib_rec_straight(abs(n)) if n<0 and n%2==0 else mfib_rec_straight(abs(n))
-    res=0-mfib_rec_bottom_up(abs(n)) if n<0 and n%2==0 else mfib_rec_bottom_up(abs(n))
+    #res=0-mfib_rec_bottom_up(abs(n)) if n<0 and n%2==0 else mfib_rec_bottom_up(abs(n))
+    res=0-mfib_rec_bottom_up_1(abs(n)) if n<0 and n%2==0 else mfib_rec_bottom_up_1(abs(n))
     print n,"TIME:",time.time()-start_t,
     return res
 
 
 
-#print "0:",mfib_rec(0)
-#print "1:",mfib_rec(1)
-#print "2:",mfib_rec(2)
-#print "3:",mfib_rec(3)
-#print "4:",mfib_rec(4)
-#print "5:",mfib_rec(5)
-#print "-5:",mfib_rec(-5)
-#print "100:",mfib_rec(100)
-#print "-100:",mfib_rec(-100)
-#print "1k:",mfib_rec(1000)
+print "0:",mfib_rec(0)
+print "1:",mfib_rec(1)
+print "2:",mfib_rec(2)
+print "3:",mfib_rec(3)
+print "4:",mfib_rec(4)
+print "5:",mfib_rec(5)
+print "-5:",mfib_rec(-5)
+print "100:",mfib_rec(100)
+print "-100:",mfib_rec(-100)
+print "1k:",mfib_rec(1000)
 # --- here is where recurtion exceeds max depth, even in mem-dp version of the recursive algorithm ---
 #print "10k:",mfib_rec(10000)
 #print "100k:",mfib_rec(100000)
@@ -61,6 +73,8 @@ def mfib_rec(n):
 mfib_rec(100000)
 print
 mfib_rec(500000)
+print
+mfib_rec(700000)
 print
 mfib_rec(1000000)
 print
